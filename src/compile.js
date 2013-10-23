ceebf8c44eec4f5f87bb06b311affb4ff828cc60
Niklas von Hertzen <niklasvh@gmail.com>
2013-10-23 22:14:10 +0300

    Basic block parsing

--- src/compile.js
+++ src/compile.js
@@ -1,13 +1,8 @@
 var Parser = require("../lib/html5-parser/lib/main.js").Parser;
 
 function Compile(template) {
-  var functionRegExp = /^function\s?\((.*)\)\s?\{(\s*)([\s\S]*)\}$/;
   var parser = new Parser(template, {type: "tree", fragment: "body"});
   parser.run();
-
-  function trim(str) {
-    return str.trim();
-  }
 
   function renderAttribute(attributes) {
     return function(attributeName) {
@@ -16,66 +11,116 @@ function Compile(template) {
   }
 
   function renderNode(node) {
-
     switch(node.nodeType) {
       case 1:
         if (node.childNodes.length) {
           return [
-            "stack.push(" + (Object.keys(node.attributes).length ? "tmp = " : "") + "append(document.createElement(" + JSON.stringify(node._tagName) + ")));",
+            "stack.push(" + (Object.keys(node.attributes).length ? "tmp = " : "") + "template.a(document.createElement(" + JSON.stringify(node._tagName) + "), stack));",
             (Object.keys(node.attributes).length ? Object.keys(node.attributes).map(renderAttribute(node.attributes)).join("\n") : ""),
             node.childNodes.map(renderNode).join(""),
             "stack.pop();"
           ].join("\n");
         } else {
-          return (Object.keys(node.attributes).length ? "tmp = " : "") + "append(document.createElement(" + JSON.stringify(node._tagName) + "));";
+          return (Object.keys(node.attributes).length ? "tmp = " : "") + "template.a(document.createElement(" + JSON.stringify(node._tagName) + "), stack);";
         }
         break;
       case 3:
-        return "append(document.createTextNode(" + JSON.stringify(node.data) + "));";
+        return "template.a(document.createTextNode(" + JSON.stringify(node.data) + "), stack);";
       case -1:
-        return "append(document.createTextNode(text('" + node.name + "', data)));";
+        return "template.a(document.createTextNode(template.t('" + node.name + "', data, context)), stack);";
+      case -2:
+        return [
+          "template.b(" + JSON.stringify(node._tagName) + ", context)",
+          node.childNodes.map(renderNode).join(""),
+          "context.pop();"
+        ].join("\n");
       default:
         return "";
     }
   }
 
-  function extractFunctionArguments(func) {
-    return func.toString().replace(functionRegExp, function() {
-      return arguments[1];
-    }).split(",").map(trim);
-  }
-
-  function extractFunctionBody(func) {
-    return func.toString().replace(functionRegExp, function() {
-      return arguments[3];
-    });
-  }
-
-
-  var functionStart = function(data, options) {
-    data = typeof(data) !== "object" ? {} : data;
-    options = options || {};
-    var document = options.document || document,
-      fragment = document.createDocumentFragment(), stack = [fragment],
-      tmp,
-      text = function(name, data) {
-        var variable = data[name];
-        return variable ? String(variable) : "";
-      },
-      resolve = function(name, data) {
-
-      },
-      append = function(node) {
-        return stack[stack.length - 1].appendChild(node);
-      };
-
-  };
-
-  var functionBody = [extractFunctionBody(functionStart)].concat(parser.constructor.childNodes.map(renderNode));
+  var functionBody = parser.constructor.childNodes.map(renderNode);
   functionBody.push("return fragment");
-  return new Function(extractFunctionArguments(functionStart), functionBody.join("\n"));
+  return new Function("template", "data", "options", "document", "fragment", "stack", "context", functionBody.join("\n"));
 }
 
+function AliveTemplate(template) {
+  this.template = template;
+}
 
+AliveTemplate.prototype.render = function(data, options) {
+  data = typeof(data) !== "object" ? {} : data;
+  options = options || {};
+  var document = options.document || document;
+  var fragment = document.createDocumentFragment();
+  return this.template(this, data, options, document, fragment, [fragment], []);
+};
 
+// append node to top-most node in stack
+AliveTemplate.prototype.a = function(node, stack) {
+  return stack[stack.length - 1].appendChild(node);
+};
+
+AliveTemplate.prototype.b = function(name, context) {
+  context.push(name);
+};
+
+AliveTemplate.prototype.t = function(name, data, context) {
+  var variable = this.lookup(data, context, name).value;
+  return variable ? String(variable) : "";
+};
+
+AliveTemplate.prototype.lookup = function(data, context, name, index) {
+  var contextLookup = data,
+    match = (name !== ".") ? data[name] : data,
+    split = name.split("."),
+    dottedName = split.length > 1,
+    stack = [],
+    matchStack = [];
+
+  if (name !== ".") {
+    name = split.pop();
+    context = context.concat(split);
+  }
+
+  for (var i = 0, len = context.length; i < len; i++) {
+    if (contextLookup[context[i]] !== undefined) {
+      contextLookup = contextLookup[context[i]];
+      stack.push(context[i]);
+      if (contextLookup[name] && name !== ".") {
+        match = contextLookup[name];
+        matchStack.push(context[i]);
+      }
+    } else if (data[context[i]] !== undefined && data[context[i]][name] && name !== "." && !dottedName) {
+      contextLookup = data[context[i]];
+      stack.push(data[context[i]]);
+      matchStack.push(data[context[i]]);
+      match = contextLookup[name];
+    }
+  }
+
+  if (index !== undefined) {
+    contextLookup = contextLookup[index];
+  }
+
+  if (name !== "." && contextLookup) {
+    if (contextLookup[name] !== undefined) {
+      return {
+        value: contextLookup[name],
+        context: stack
+      };
+    } else {
+      return {
+        value: match,
+        context: matchStack
+      };
+    }
+  }
+  return {
+    value: contextLookup,
+    context: stack
+  };
+};
+
+exports.AliveTemplate = AliveTemplate;
 exports.Compile = Compile;
--- test/nodeunit.js
+++ test/nodeunit.js
@@ -1,19 +1,20 @@
 var yaml = require("js-yaml"),
     fs = require("fs"),
     Compile = require("../src/compile.js").Compile,
+    AliveTemplate = require("../src/compile.js").AliveTemplate,
     DOMSerializer = require("./DOMSerializer.js").DOMSerializer;
 
 var testPath = "test/";
-var tests = ['basic.yml'];
+var tests = ['basic.yml', 'interpolation.yml'];
 
 tests.forEach(function(filename) {
   exports[filename] = function(test) {
     var testData = yaml.safeLoad(fs.readFileSync(testPath + filename).toString());
     test.expect(testData.tests.length);
     testData.tests.forEach(function(testCase) {
-      var compiledTemplate = Compile(testCase.template);
+      var compiledTemplate = new AliveTemplate(Compile(testCase.template));
       var options = {document:  new DOMSerializer()};
-      test.equal(compiledTemplate(testCase.data || {}, options).toString(), testCase.expected, testCase.name);
+      test.equal(compiledTemplate.render(testCase.data || {}, options).toString(), testCase.expected, testCase.name);
     });
     test.done();
   };
